9d7344830d5b9be0984b6da91603b98e29d32a93
Aldair André <aldairandre99@gmail.com>
2022-04-20 12:11:15 +0100

    primeiro teste para conhecer estrutura da jasmine

---
+++ spec/my-test-script/soma-test.js
@@ -0,0 +1,17 @@
+describe('Soma dois numero',function (){
+    const Calculadora = require('../../lib/jasmine_examples/my-script/Calculadora')
+
+    let calculadora;
+
+    /**Incializando a class Calculadora */
+    beforeEach(function() {
+        calculadora = new Calculadora();
+    });
+
+
+    /**Testando se a funca retorna o valor 2 */
+    it('passados para soma',function(){
+        calculadora.soma(1,1)
+        expect(calculadora.result).toBe(2)
+    })
+})
